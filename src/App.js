413b5994c5acafd8d0794b1ec3a564c637c28509
SAIF <zarraisaifedine@gmail.com>
2021-02-14 17:12:01 +0100

    final

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Profile from './profile/Profile'
 
-const data = {fullName : "zarrai saif", bio: "bla bla", profession: "full stack dev",
- pictures: 'http://via.placeholder.com/200x100', handlename: (fullName) =>{alert(fullName);}}
+const data = {
+  fullName : "zarrai saif", 
+  bio: "bla bla", 
+  profession: "full stack dev",
+ pictures: 'http://via.placeholder.com/200x100', 
+ handlename: (fullName) =>{alert('hello '+fullName);}
+}
 
 function App() {
   return (
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,19 +1,22 @@
 import React from 'react'
 
 const Profile = (props) => {
+    const style = {textAlign:'center'}
     return (
-        <>
-            <h1>{props.data.fullName}</h1>
+        <div style={style}>
+            <h1>{props.fullName}</h1>
             <h1>{props.data.profession}</h1>
             <h1>{props.data.bio}</h1>
                  {props.children}
+            <br/>
             <button type="button" className="btn btn-primary" onClick={() => { props.data.handlename(props.data.fullName) }}>Alert</button>
             <p>{props.s}</p>
-        </>
+        </div>
     )
 }
 
 Profile.defaultProps = {
+    fullName : 'zarrai saif'
     
 }
 
